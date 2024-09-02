75bfc6ab6c590e82c438af034562ea28a1193ec6
lucadeoliveira2 <lucadeoliveira2@gmail.com>
2024-09-02 12:51:36 +0100

    First Commit

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import pandas as pd
+from pandas import *
 import streamlit as st
 
 st.write('hello')
 st.write('he;;0')
+
+df = read_csv('/Users/apple/Downloads/Coding and Machine Learning/Python for Data Analysis/StockAnalysisProject/sp500_companies.csv')
+st.write(df)
